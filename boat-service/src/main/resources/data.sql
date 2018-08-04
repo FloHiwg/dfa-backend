9d38f9da069d3fe9767a18b25455e87b3e0f25e3
Heiwig Florian <Florian.Hiwg@gmail.com>
2018-08-04 23:35:32 +0200

    added application properties with basic informations

---
+++ boat-service/src/main/resources/data.sql
@@ -0,0 +1,8 @@
+insert into boat(id, boat_name, club_id, boat_type,boat_slot,boat_rigger,boat_cox)
+values(10001,'Musen Sohn', 10001, 'RACE_BOAT','SKIFF', 'SCULL', 'COXED');
+
+insert into boat(id, boat_name, club_id, boat_type,boat_slot,boat_rigger,boat_cox)
+values(10002,'Musen Sohn2', 10001, 'RACE_BOAT','SKIFF', 'SCULL', 'COXED');
+
+insert into boat(id, boat_name, club_id, boat_type,boat_slot,boat_rigger,boat_cox)
+values(10003,'Musen Sohn3', 10001, 'RACE_BOAT','SKIFF', 'SCULL', 'COXED');
