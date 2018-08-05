42fbcc0d961efb5b930d2dc1fc6818745b85e22a
Heiwig Florian <Florian.Hiwg@gmail.com>
2018-08-05 13:05:42 +0200

    added init version of club service

---
+++ club-service/src/main/java/com/heiwig/dfa/clubservice/ClubServiceApplication.java
@@ -0,0 +1,12 @@
+package com.heiwig.dfa.clubservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClubServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClubServiceApplication.class, args);
+	}
+}
