b809b48456a689e957913bfc9005ba22c9f3e7ad
Heiwig Florian <Florian.Hiwg@gmail.com>
2018-08-05 12:18:33 +0200

    added zuul

---
+++ netflix-zuul-api-gateway-server/src/main/java/com/heiwig/dfa/netflixzuulapigatewayserver/NetflixZuulApiGatewayServerApplication.java
@@ -0,0 +1,12 @@
+package com.heiwig.dfa.netflixzuulapigatewayserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetflixZuulApiGatewayServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NetflixZuulApiGatewayServerApplication.class, args);
+	}
+}
