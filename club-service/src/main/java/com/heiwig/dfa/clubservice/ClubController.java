6a0d179386d519864322327e5aec6f7ae44a8f0b
Heiwig Florian <Florian.Hiwg@gmail.com>
2018-08-05 13:34:45 +0200

    added get endpoint to club service to retrieve club by id

---
+++ club-service/src/main/java/com/heiwig/dfa/clubservice/ClubController.java
@@ -0,0 +1,40 @@
+package com.heiwig.dfa.clubservice;
+
+import com.heiwig.dfa.clubservice.beans.Club;
+import com.heiwig.dfa.clubservice.beans.ClubRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.Optional;
+
+public class ClubController {
+
+    Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    //Can be used to return port for testing and logging purposes
+    @Autowired
+    private Environment environment;
+
+    @Autowired
+    private ClubRepository repository;
+
+    @GetMapping("/club/id/{id}")
+    public Club retrieveClubById(@PathVariable long id) {
+        logger.info("Get boat with id: {}", id);
+
+        Optional<Club> club = repository.findById(id);
+
+        logger.info("Retrieve club by id: {}", club);
+
+        if(club.isPresent()) {
+            return club.get();
+        } else {
+            //TODO: responed with error that id couldn't be found
+        }
+        return null;
+    }
+}
